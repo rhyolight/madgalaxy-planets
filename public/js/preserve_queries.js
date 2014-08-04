91fdaa8c532be0f4e38cd3828f65076c4eae3192
Brady Brown <bobrown101@gmail.com>
2014-08-04 10:13:07 -0400

    changed pagination so it now shows link on hover just to keep users expectation

--- public/js/preserve_queries.js
+++ public/js/preserve_queries.js
@@ -1,35 +1,37 @@
 function preserveTags(tagToAdd) {
-    var urlParams;
-    (window.onpopstate = function() {
-        var match,
-            pl = /\+/g, // Regex for replacing addition symbol with a space
-            search = /([^&=]+)=?([^&]*)/g,
-            decode = function(s) {
-                return decodeURIComponent(s.replace(pl, " "));
-            },
-            query = window.location.search.substring(1);
+  var urlParams;
+  (window.onpopstate = function() {
+    var match,
+      pl = /\+/g, // Regex for replacing addition symbol with a space
+      search = /([^&=]+)=?([^&]*)/g,
+      decode = function(s) {
+        return decodeURIComponent(s.replace(pl, " "));
+      },
+      query = window.location.search.substring(1);
 
-        urlParams = {};
-        while (match = search.exec(query))
-            urlParams[decode(match[1])] = decode(match[2]);
-    })();
+    urlParams = {};
+    while (match = search.exec(query))
+      urlParams[decode(match[1])] = decode(match[2]);
+  })();
 
-    console.log('here are the current url parameters: ' + JSON.stringify(urlParams));
-    var newUri = '?';
-    for (paramName in urlParams) {
-        if (paramName != 'pageNum') {
-            newUri = newUri + paramName + '=' + urlParams[paramName] + '&';
-            console.log(paramName);
-        }
+  console.log('here are the current url parameters: ' + JSON.stringify(urlParams));
+  var newUri = '?';
+  for (var paramName in urlParams) {
+    if (paramName != 'pageNum') {
+      newUri = newUri + paramName + '=' + urlParams[paramName] + '&';
+      console.log(paramName);
     }
-    newUri = newUri + tagToAdd;
-    console.log(newUri)
-    return newUri
-}
+  }
+  newUri = newUri + tagToAdd;
+  console.log(newUri);
+  return newUri;
+};
 preserveTags('pageNum=1000');
 
 
-
 $('#pageForward').click(function(event) {
-    window.location.href = "/search/" + preserveTags('pageNum=' + $('#pageForward').attr('data-pageNum'));
+  window.location.href = preserveTags('pageNum=' + $('#pageForward').attr('data-pageNum'));
+});
+$('#pageBackward').click(function(event) {
+  window.location.href = preserveTags('pageNum=' + $('#pageBackward').attr('data-pageNum'));
 });
